44c426857175f2af9abe70a7684546578f28eb51
Adi Eyal <adi@openup.org.za>
2020-05-08 18:48:58 +0200

    Added setup files

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = "dictutils",
+  packages = ["dictutils"],
+  version = "0.1",
+  license="MIT",
+  description = "A collection of useful tools for manipulating dictionaries.",
+  author = "Adi Eyal",
+  author_email = "adi@openup.org.za",
+  url = "https://github.com/adieyal/dictutils",
+  download_url = "https://github.com/adieyal/dictutils/archive/v_01.tar.gz",
+  keywords = ["dictionary", "utility"],
+  install_requires = [],
+  classifiers=[
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "Topic :: Software Development :: Build Tools",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Topic :: Software Development",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+  ],
+)
