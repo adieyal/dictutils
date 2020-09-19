ed093556195b3370a985ceea9bdee2dfac6ea0a9
Adi Eyal <adi@openup.org.za>
2020-09-19 22:57:31 +0200

    Removed old annotation

--- dictutils/qsdict.py
+++ dictutils/qsdict.py
@@ -1,7 +1,7 @@
 from collections import OrderedDict
 from typing import Mapping, Iterable, Dict 
 
-def qsdict(qs: Iterable[Mapping], *args: Any) -> Dict:
+def qsdict(qs: Iterable[Mapping], *args) -> Dict:
     """
     Convert a query string to a nested dict
     :param qs: The query string from which the array will be created. qs can also be an object and args are all attributes on that object.
